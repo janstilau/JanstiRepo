d0e738e9c54994636af38924bdbed013f54625e7
janstilau <janstilau@gmail.com>
2016-11-24 15:14:11 +0800

    封装系统icloud方法,添加自己的回调

---
+++ Demo/cloudDemo/reInputCloudDemo/reInputCloudDemo/City.swift
@@ -0,0 +1,23 @@
+//
+//  City.swift
+//  reInputCloudDemo
+//
+//  Created by jansti on 16/11/24.
+//  Copyright © 2016年 jansti. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+let cityName = "name"
+let cityText = "text"
+let cityPicture = "picture"
+private let kCitiesSourcePlist = "Cities"
+
+class City {
+    
+    
+    init(record: CKRecord){
+        
+    }
+}
--- Demo/cloudDemo/reInputCloudDemo/reInputCloudDemo/CloudKitManager.swift
+++ Demo/cloudDemo/reInputCloudDemo/reInputCloudDemo/CloudKitManager.swift
@@ -13,6 +13,8 @@ import CloudKit
 private let recordType = "Cities"
 
 
+
+
 /*
  public enum CKDatabaseScope : Int {
  
@@ -26,14 +28,8 @@ private let recordType = "Cities"
  
  */ // 这上面,就是用到了系统保留关键字,但是这些关键字又和业务相关,所以用''进行了修饰.
 
-class City {
-    
-    
-    init(record: CKRecord){
-        
-    }
-}
 
+// 这个工具类基本就是封装了icloud的系统方法,icloud的方法一般都有一个提交给icloud的回调,然后在回调里面,执行自己定义的block
 final class CloudKitManager {
     
     fileprivate init(){
@@ -47,7 +43,7 @@ final class CloudKitManager {
     
     // MARK: Retrieve existing records
     static func fetchAllCities(_ completion:@escaping (_ records: [City]?,_ error: NSError?) -> Void){
-        let predicate = NSPredicate.init(value: true)
+        let predicate = NSPredicate.init(value: true)// evalueatePrediate总是返回true
         let query = CKQuery.init(recordType: recordType, predicate: predicate)
         // 根据谓词来进行icould的查找.
         publicCloudDatabase.perform(query, inZoneWith: nil, completionHandler: {
@@ -63,9 +59,87 @@ final class CloudKitManager {
         })
     }// 这里我们看到,所有的查找,插入,修改都要在调用cloudkit的方法,然后在block的回调里面执行后续的操作,也就是说,都要确保在icould里面完成了修改,才能继续操作.
     
-    func method(hander: (Int, Double) -> ()) {
+    //MARK: add new record .建立record, set value, save value, complete
+    static func createRecord(_ recordData: [String: String], completion: @escaping (CKRecord?, NSError?) -> Void){
         
+        let record = CKRecord(recordType: recordType)
         
+        for (key, value) in recordData {
+            if key == cityPicture {
+                
+                if let path = Bundle.main.path(forResource: value, ofType: "jpg") {
+                    do{
+                        let data = try Data.init(contentsOf: URL.init(fileURLWithPath: path), options: Data.ReadingOptions.mappedIfSafe)
+                        record.setValue(data, forKey: key)
+                    }catch let error {
+                        print(error)
+                    }
+                }
+            }else {
+                record.setValue(value, forKey: key)
+            }
+        }
+        
+        publicCloudDatabase.save(record, completionHandler: { (savedRecord, error) in
+            DispatchQueue.main.async {
+                completion(record, error as? NSError)
+            }
+        })
+    }
+    
+    
+    
+    //MARK: update value by recordID
+    static func updateRecord(_ recordId: String, text: String, completion: @escaping (CKRecord?, NSError?) -> Void ){
+        
+        let recordID = CKRecordID.init(recordName: recordId)
+        publicCloudDatabase.fetch(withRecordID: recordID, completionHandler: {
+            (updatedRecord, error) in
+            guard let record = updatedRecord else {
+                DispatchQueue.main.async {
+                    completion(nil, error as? NSError)
+                }
+                return
+            }
+            record.setValue(text, forKey: cityText)
+            self.publicCloudDatabase.save(record, completionHandler: {
+                (savedReocrd, error ) in
+                DispatchQueue.main.async {
+                    completion(savedReocrd, error as? NSError)
+                }
+            })
+        })
+    }
+    
+    
+    // MARK: remove the record
+    static func removeRecord(_ recordId: String, completion: @escaping (String?, NSError?) -> Void){
+        
+        let recordId = CKRecordID.init(recordName: recordId)
+        publicCloudDatabase.delete(withRecordID: recordId, completionHandler:
+            { (deletedRecordId, error) in
+                DispatchQueue.main.async {
+                    completion(deletedRecordId?.recordName, error as NSError?)
+                }
+        })
+    }
+    
+    
+    //MARK: check that user is logged
+    
+    static func checkLoginStatus(_ handler: @escaping (Bool) -> Void) {
+        
+        CKContainer.default().accountStatus { (accountStatus, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            switch accountStatus {
+            case .available:
+                handler(true)
+            default:
+                handler(false)
+            }
+        }
     }
     
 }
@@ -121,6 +195,55 @@ final class CloudKitManager {
  }];
 
 */
+
+
+/*  Apple 创建保存record
+ CKRecordID *artworkRecordID = [[CKRecordID alloc] initWithRecordName:@"115"];
+ CKRecord *artworkRecord = [[CKRecord alloc] initWithRecordType:@"Artwork" recordID:artworkRecordID];
+ artworkRecord[@"title" ] = @"MacKerricher State Park";
+ artworkRecord[@"artist"] = @"Mei Chen";
+ artworkRecord[@"address"] = @"Fort Bragg, CA";
+ 
+ To get the public database:
+ CKContainer *myContainer = [CKContainer defaultContainer];
+ CKDatabase *publicDatabase = [myContainer publicCloudDatabase];
+ 
+ To get the private database:
+ CKContainer *myContainer = [CKContainer defaultContainer];
+ CKDatabase *privateDatabase = [myContainer privateCloudDatabase];
+ 
+ To get a custom container:
+ CKContainer *myContainer = [CKContainer containerWithIdentifier:@"iCloud.com.example.ajohnson.GalleryShared"];
+
+ [publicDatabase saveRecord:artworkRecord completionHandler:^(CKRecord *artworkRecord, NSError *error){
+ if (!error) {
+ // Insert successfully saved record code
+ }
+ else {
+ // Insert error handling
+ }
+ }];
+ 
+ */
+
+
+/* Apple alert权限
+ 
+ [[CKContainer defaultContainer] accountStatusWithCompletionHandler:^(CKAccountStatus accountStatus, NSError *error) {
+ if (accountStatus == CKAccountStatusNoAccount) {
+ UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Sign in to iCloud"
+ message:@"Sign in to your iCloud account to write records. On the Home screen, launch Settings, tap iCloud, and enter your Apple ID. Turn iCloud Drive on. If you don't have an iCloud account, tap Create a new Apple ID."
+ preferredStyle:UIAlertControllerStyleAlert];
+ [alert addAction:[UIAlertAction actionWithTitle:@"Okay"
+ style:UIAlertActionStyleCancel
+ handler:nil]];
+ [self presentViewController:alert animated:YES completion:nil];
+ }
+ else {
+ // Insert your just-in-time schema code here
+ }
+ }];
+ */
 
 
 
