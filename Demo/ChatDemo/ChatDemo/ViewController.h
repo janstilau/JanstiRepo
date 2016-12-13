80dade4579934da20aad4b3a5f7b5046a8064921
janstilau <janstilau@gmail.com>
2016-12-13 09:48:00 +0800

    sync

---
+++ Demo/ChatDemo/ChatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatDemo
+//
+//  Created by jansti on 16/12/8.
+//  Copyright © 2016年 jansti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/ChatDemo/ChatDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChatDemo
+//
+//  Created by jansti on 16/12/8.
+//  Copyright © 2016年 jansti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
