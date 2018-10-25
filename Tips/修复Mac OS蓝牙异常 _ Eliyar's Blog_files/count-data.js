8c29a37183983b64cb932f241f5c4a06bbe6a242
JustinLau <janstilau@gmail.com>
2018-10-25 09:39:21 +0800

    sync

---
+++ Tips/修复Mac OS蓝牙异常 _ Eliyar's Blog_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"\u53ca","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"fix_mac_os_bluetooth_error\/","comments":2}]});
+}
