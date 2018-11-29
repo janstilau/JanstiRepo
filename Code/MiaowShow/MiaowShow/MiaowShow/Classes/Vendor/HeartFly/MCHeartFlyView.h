a17b74143572d6c9e6a5ac8a72199a71adbe7a88
JustinLau <janstilau@gmail.com>
2018-11-29 16:42:46 +0800

    分析 弹幕系统

---
+++ Code/MiaowShow/MiaowShow/MiaowShow/Classes/Vendor/HeartFly/MCHeartFlyView.h
@@ -0,0 +1,19 @@
+//
+//  MCHeartFlyView.h
+//  MiaowShow
+//
+//  Created by JustinLau on 2018/11/29.
+//  Copyright © 2018年 ALin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MCHeartFlyView : UIView
+
+-(void)animateInView:(UIView *)view;
+
+@end
+
+NS_ASSUME_NONNULL_END
