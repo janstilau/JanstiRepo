0ab77c8332d832b9929241a719a887aaea753134
JustinLau <janstilau@gmail.com>
2019-01-14 21:49:26 +0800

    sync

---
+++ Code/TouchDemo/TouchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchDemo
+//
+//  Created by JustinLau on 2019/1/14.
+//  Copyright © 2019年 JustinLau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
