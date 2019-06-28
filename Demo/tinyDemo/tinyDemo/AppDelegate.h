b15e98c9f472e691f3d8885e3bcbeefa2d8d5ae5
JustinLau <janstilau@gmail.com>
2019-06-28 10:27:28 +0800

    sync

---
+++ Demo/tinyDemo/tinyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tinyDemo
+//
+//  Created by JustinLau on 2019/6/27.
+//  Copyright © 2019 JustinLau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/tinyDemo/tinyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tinyDemo
+//
+//  Created by JustinLau on 2019/6/27.
+//  Copyright © 2019 JustinLau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
