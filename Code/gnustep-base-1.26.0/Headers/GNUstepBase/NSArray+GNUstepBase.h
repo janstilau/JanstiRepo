aaccfab2a240d1ffc0f7491ac4b8d0265af34e51
JustinLau <janstilau@gmail.com>
2019-10-01 12:24:33 +0800

    no message

--- Code/gnustep-base-1.26.0/Headers/GNUstepBase/NSArray+GNUstepBase.h
+++ Code/gnustep-base-1.26.0/Headers/GNUstepBase/NSArray+GNUstepBase.h
@@ -1,29 +1,3 @@
-/** Declaration of extension methods for base additions
-
-   Copyright (C) 2003-2010 Free Software Foundation, Inc.
-
-   Written by:  Richard Frith-Macdonald <rfm@gnu.org>
-   and:         Adam Fedor <fedor@gnu.org>
-
-   This file is part of the GNUstep Base Library.
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-   
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free
-   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02111 USA.
-
-*/
-
 #ifndef	INCLUDED_NSArray_GNUstepBase_h
 #define	INCLUDED_NSArray_GNUstepBase_h
 
@@ -51,21 +25,21 @@ extern "C" {
  * </p>
  */
 - (NSUInteger) insertionPosition: (id)item
-		   usingFunction: (NSComparisonResult (*)(id, id, void *))sorter
-		         context: (void *)context;
+                   usingFunction: (NSComparisonResult (*)(id, id, void *))sorter
+                         context: (void *)context;
 
 /* <p>Method for working with sorted arrays - use a binary chop
  * to determine the insertion location for an object.  If equal objects
  * already exist in the array, they will be located immediately before
  * the insertion position.
- * </p> 
+ * </p>
  * <p>The selector identifies a method returning NSOrderedAscending if
  * the receiver is 'less than' the argument, and NSOrderedDescending if
  * it is greate.
  * </p>
  */
 - (NSUInteger) insertionPosition: (id)item
-		   usingSelector: (SEL)comp;
+                   usingSelector: (SEL)comp;
 @end
 
 #endif	/* OS_API_VERSION */
