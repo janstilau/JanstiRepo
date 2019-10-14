d1f58c6acbe48cac57758505048465118eeb1e27
JustinLau <janstilau@gmail.com>
2019-10-14 23:07:38 +0800

    sync

--- Code/gnustep-base-1.26.0/Source/NSString.m
+++ Code/gnustep-base-1.26.0/Source/NSString.m
@@ -1,36 +1,3 @@
-/** Implementation of GNUSTEP string class
-   Copyright (C) 1995-2012 Free Software Foundation, Inc.
-
-   Written by:  Andrew Kachites McCallum <mccallum@gnu.ai.mit.edu>
-   Date: January 1995
-
-   Unicode implementation by Stevo Crvenkovski <stevo@btinternet.com>
-   Date: February 1997
-
-   Optimisations by Richard Frith-Macdonald <richard@brainstorm.co.uk>
-   Date: October 1998 - 2000
-
-   This file is part of the GNUstep Base Library.
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free
-   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02111 USA.
-
-   <title>NSString class reference</title>
-   $Date$ $Revision$
-*/
-
 /* Caveats:
 
    Some implementations will need to be changed.
