6eee1a99938a54024bbddced65ddabd672dbd4b8
JustinLau <janstilau@gmail.com>
2019-11-22 20:01:05 +0800

    sync

---
+++ swiftPrac.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let maximumNumberOfLoginAttemptes = 10
+var welcomeMessage: String
+welcomeMessage = "heheda"
+
+print(welcomeMessage)
+
+Int.max
+UInt16.max
+UInt16.min
+print(UInt.max)
+print(Int.max)
+
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+
+
+
