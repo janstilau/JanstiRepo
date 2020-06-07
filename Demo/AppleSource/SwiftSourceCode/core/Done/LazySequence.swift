65a13895d508abce5611f53e126689f4766c5bfa
JustinLau <janstilau@gmail.com>
2020-06-07 12:38:18 +0800

    Lazy Sequence Analysis

--- Demo/AppleSource/SwiftSourceCode/core/Done/LazySequence.swift
+++ Demo/AppleSource/SwiftSourceCode/core/Done/LazySequence.swift
@@ -28,6 +28,7 @@
 /// return a new array.  Using the `lazy` property gives the standard
 /// library explicit permission to store the closure and the sequence
 /// in the result, and defer computation until it is needed.
+///  这里讲的很明白, 作为 lazy sequence, 只有当访问到它的数据的时候,  才会进行相应数据的生成操作.
 ///
 /// To add new lazy sequence operations, extend this protocol with
 /// methods that return lazy wrappers that are themselves
@@ -60,19 +61,28 @@
 /// we can build a sequence that lazily computes the elements in the
 /// result of `scan`:
 ///
+/// 作为一个 Lazy Iterator, 它存储了原有的Sequence 的 iterator. 存储了原有的值, 存储了原有值和新值操作的 block.
 ///     struct LazyScanIterator<Base: IteratorProtocol, ResultElement>
 ///       : IteratorProtocol {
-///       mutating func next() -> ResultElement? {
-///         return nextElement.map { result in
-///           nextElement = base.next().map { nextPartialResult(result, $0) }
-///           return result
-///         }
-///       }
+///
 ///       var nextElement: ResultElement? // The next result of next().
 ///       var base: Base                  // The underlying iterator.
 ///       let nextPartialResult: (ResultElement, Base.Element) -> ResultElement
+///
+///       mutating func next() -> ResultElement? {
+///         return nextElement.map { result in // 这里的 result, 是 nextElement 的 wrappedValue
+///           nextElement = base.next().map { nextPartialResult(result, $0) } // 这里的 $0 是 base.next() 的 wrappedValue
+///           return result
+///         }
+///       }
 ///     }
-///     
+///
+///
+/*
+ 对于一个 LazyScanSequence 来说, 生成它的对象的时候, 仅仅是对于原始 sequence, iterator, 还有 transform 闭包的一份记录而已.
+ 它的值在 access 的过程中, 才会动态的生成. 利用的就是它之前记录的那几个值.
+ Lazy 可以避免在创建的过程中进行计算.
+*/
 ///     struct LazyScanSequence<Base: Sequence, ResultElement>
 ///       : LazySequenceProtocol // Chained operations on self are lazy, too
 ///     {
@@ -128,6 +138,12 @@
 ///   [We don't recommend that you use `map` this way, because it
 ///   creates and discards an array. `sum` would be better implemented
 ///   using `reduce`].
+
+/*
+ LazySequenceProtocol : Sequence
+ 标明, LazySequenceProtocol 的使用方式要完全和 Sequence 相同的.
+ 它的 primitiveMethod 里面, 没有任何对于能力的要求.
+ */
 public protocol LazySequenceProtocol: Sequence {
   /// A `Sequence` that can contain the same elements as this one,
   /// possibly with a simpler type.
@@ -176,7 +192,11 @@ extension LazySequenceProtocol where Elements: LazySequenceProtocol {
 /// on which some operations such as `map` and `filter` are
 /// implemented lazily.
 ///
+///
 /// - See also: `LazySequenceProtocol`
+/*
+ Lazy, 首先要记录原来的 sequence.
+ */
 @frozen // lazy-performance
 public struct LazySequence<Base: Sequence> {
   @usableFromInline
@@ -241,3 +261,13 @@ extension Sequence {
     return LazySequence(_base: self)
   }
 }
+
+/*
+ 整个文件里面, 没有说明 lazy 的具体的实现思路是什么.
+ Lazy 到底怎么变得 lazy, 是各个开发人员, 根据自己的业务需求, 自己实现的.
+ 注释中, 有一个 lazy Scan 的实现方式.
+ 基本的实现思路是, 在 lazySequence 的 iterator 立马, next 除了访问baseIterator的 next 的返回结果以外, 还要对于返回结果进行一次操作.
+ 而这个操作, 是迭代的过程中才生产出来的.
+ 也就是说, 生产一个 lazySequence, 附带相应的 block 操作, 不会立马进行 block 的调用.
+ 这个各个 drop, Suffix sequence 的操作是一样的. 都是通过 baseiterator 进行取值, 只是在这个取值的过程中, 进行了自定义.
+ */
