47e1ed821f8ceafe7bc85b53172301bad7b6112a
JustinLau <janstilau@gmail.com>
2020-07-10 13:21:19 +0800

    Sync

--- canon/CartoonSwift/CartoonSwift/BinaryTree/MaxTreeDepth.swift
+++ canon/CartoonSwift/CartoonSwift/BinaryTree/MaxTreeDepth.swift
@@ -183,3 +183,147 @@ class BuildTreeFromInPostOrder {
  -1000 <= node.val <= 1000
  */
 
+
+/*
+ Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+ According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+
+  
+
+ Example 1:
+
+ Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ Output: 3
+ Explanation: The LCA of nodes 5 and 1 is 3.
+ Example 2:
+
+ Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ Output: 5
+ Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+  
+
+ Note:
+
+ All of the nodes' values will be unique.
+ p and q are different and both values will exist in the binary tree.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class CommonAncestor {
+    
+    func fillPath(_ root: TreeNode, target: TreeNode, path: inout [TreeNode], flag: inout Bool) {
+        path.append(root)
+        if root.val == target.val {
+            flag = true
+            return
+        }
+        if let leftNode = root.left {
+            fillPath(leftNode, target: target, path: &path, flag: &flag)
+            if flag {
+                return
+            }
+        }
+        if let rightNode = root.right {
+            fillPath(rightNode, target: target, path: &path, flag: &flag)
+            if flag {
+                return
+            }
+        }
+        path.removeLast()
+    }
+    
+    func pathForNode(_ root: TreeNode, target: TreeNode) -> [TreeNode] {
+        var result = [TreeNode]()
+        var flag = false
+        fillPath(root, target: target, path: &result, flag: &flag)
+        return result
+    }
+    
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        guard let root = root else {
+            return nil
+        }
+        guard let p = p else {
+            return nil
+        }
+        guard let q = q else {
+            return nil
+        }
+        let pPath = pathForNode(root, target: p)
+        let qPath = pathForNode(root, target: q)
+        for aNode in pPath.reversed() {
+            if qPath.contains(aNode) {
+                return aNode
+            }
+        }
+        return nil
+    }
+}
+
+
+/*
+ 
+ 先序遍历.
+   1
+  / \
+ 2   3
+    / \
+   4   5
+ 
+ 1, 2, null, null, 3, 4, null, null, 5, null, null
+        1
+     2
+ null null  3
+        4           5
+    null null   null null
+ 
+ class Codec {
+ public:
+      string serialize(TreeNode* root) {
+         ostringstream out;
+         serialize(root, out);
+         return out.str();
+     }
+     // Decodes your encoded data to tree.
+     TreeNode* deserialize(string data) {
+         istringstream in(data);
+         return deserialize(in);
+     }
+ private:
+     void serialize(TreeNode *root, ostringstream &out) {
+         if (root) {
+             out << root->val << ' ';
+             serialize(root->left, out);
+             serialize(root->right, out);
+         } else {
+             out << "# ";
+         }
+     }
+     TreeNode* deserialize(istringstream &in) {
+         string val;
+         in >> val;
+         if (val == "#") return nullptr;
+         TreeNode *root = new TreeNode(stoi(val));
+         root->left = deserialize(in);
+         root->right = deserialize(in);
+         return root;
+     }
+ };
+ */
