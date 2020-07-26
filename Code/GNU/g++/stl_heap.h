e466606a350c3161ce910d859bfde116db7971a5
JustinLau <janstilau@gmail.com>
2020-07-27 00:40:40 +0800

    sync

--- Code/GNU/g++/stl_heap.h
+++ Code/GNU/g++/stl_heap.h
@@ -7,6 +7,40 @@ __STL_BEGIN_NAMESPACE
 #pragma set woff 1209
 #endif
 
+/*
+ 向 fisrt, last 中, 添加元素, 然后堆化.
+ */
+template <class RandomAccessIterator>
+inline void push_heap(RandomAccessIterator first, RandomAccessIterator last) {
+    __push_heap_aux(first, last, distance_type(first), value_type(first));
+}
+
+template <class RandomAccessIterator, class Compare>
+inline void push_heap(RandomAccessIterator first, RandomAccessIterator last,
+                      Compare comp) {
+    __push_heap_aux(first, last, comp, distance_type(first), value_type(first));
+}
+
+template <class RandomAccessIterator, class Distance, class T>
+inline void __push_heap_aux(RandomAccessIterator first,
+                            RandomAccessIterator last, Distance*, T*) {
+    __push_heap(first, Distance((last - first) - 1), Distance(0),
+                T(*(last - 1)));
+}
+
+template <class RandomAccessIterator, class Compare, class Distance, class T>
+inline void __push_heap_aux(RandomAccessIterator first,
+                            RandomAccessIterator last,
+                            Compare comp, // 自定义比较函数
+                            Distance*, T*) {
+    __push_heap(first, Distance((last - first) - 1), Distance(0),
+                T(*(last - 1)),
+                comp);
+}
+
+/*
+ 插入元素后的堆化. 可以看到, 是通过同父节点的比较, 从叶子节点开始堆化的.
+ */
 template <class RandomAccessIterator, class Distance, class T>
 void __push_heap(RandomAccessIterator first, Distance holeIndex,
                  Distance topIndex, T value) {
@@ -19,21 +53,10 @@ void __push_heap(RandomAccessIterator first, Distance holeIndex,
     *(first + holeIndex) = value;
 }
 
-template <class RandomAccessIterator, class Distance, class T>
-inline void __push_heap_aux(RandomAccessIterator first,
-                            RandomAccessIterator last, Distance*, T*) {
-    __push_heap(first, Distance((last - first) - 1), Distance(0),
-                T(*(last - 1)));
-}
-
-template <class RandomAccessIterator>
-inline void push_heap(RandomAccessIterator first, RandomAccessIterator last) {
-    __push_heap_aux(first, last, distance_type(first), value_type(first));
-}
-
 template <class RandomAccessIterator, class Distance, class T, class Compare>
 void __push_heap(RandomAccessIterator first, Distance holeIndex,
-                 Distance topIndex, T value, Compare comp) {
+                 Distance topIndex, T value,
+                 Compare comp) {
     Distance parent = (holeIndex - 1) / 2;
     while (holeIndex > topIndex && comp(*(first + parent), value)) {
         *(first + holeIndex) = *(first + parent);
@@ -43,19 +66,60 @@ void __push_heap(RandomAccessIterator first, Distance holeIndex,
     *(first + holeIndex) = value;
 }
 
-template <class RandomAccessIterator, class Compare, class Distance, class T>
-inline void __push_heap_aux(RandomAccessIterator first,
-                            RandomAccessIterator last, Compare comp,
-                            Distance*, T*) {
-    __push_heap(first, Distance((last - first) - 1), Distance(0),
-                T(*(last - 1)), comp);
+/*
+ 移除堆中头结点.
+ */
+
+template <class RandomAccessIterator>
+inline void pop_heap(RandomAccessIterator first, RandomAccessIterator last) {
+    __pop_heap_aux(first, last, value_type(first));
 }
 
+
 template <class RandomAccessIterator, class Compare>
-inline void push_heap(RandomAccessIterator first, RandomAccessIterator last,
-                      Compare comp) {
-    __push_heap_aux(first, last, comp, distance_type(first), value_type(first));
+inline void pop_heap(RandomAccessIterator first, RandomAccessIterator last,
+                     Compare comp) {
+    __pop_heap_aux(first, last, value_type(first), comp);
 }
+
+template <class RandomAccessIterator, class T>
+inline void __pop_heap_aux(RandomAccessIterator first,
+                           RandomAccessIterator last, T*) {
+    __pop_heap(first, last - 1, last - 1, T(*(last - 1)), distance_type(first));
+}
+
+template <class RandomAccessIterator, class T, class Compare>
+inline void __pop_heap_aux(RandomAccessIterator first,
+                           RandomAccessIterator last, T*,
+                           Compare comp) {
+    __pop_heap(first, last - 1, last - 1, T(*(last - 1)), comp,
+               distance_type(first));
+}
+
+/*
+ First 就是该移除的对象, 先赋值到 result 中去. 然后进行调整.
+ */
+template <class RandomAccessIterator, class T, class Distance>
+inline void __pop_heap(RandomAccessIterator first, RandomAccessIterator last,
+                       RandomAccessIterator result, T value, Distance*) {
+    *result = *first;
+    __adjust_heap(first, Distance(0), Distance(last - first), value);
+}
+
+template <class RandomAccessIterator, class T, class Compare, class Distance>
+inline void __pop_heap(RandomAccessIterator first, RandomAccessIterator last,
+                       RandomAccessIterator result, T value,
+                       Compare comp,
+                       Distance*) {
+    *result = *first;
+    __adjust_heap(first, Distance(0), Distance(last - first), value, comp);
+}
+
+/*
+ 以下函数不好理解. 还是用原来的自己的思路进行理解吧.
+ push, 插入到末尾, 然后从叶子节点开始堆化. 从下往上堆化
+ pop, 头尾互换, 然后从头开始往下堆化.
+ */
 
 template <class RandomAccessIterator, class Distance, class T>
 void __adjust_heap(RandomAccessIterator first, Distance holeIndex,
@@ -76,23 +140,6 @@ void __adjust_heap(RandomAccessIterator first, Distance holeIndex,
     __push_heap(first, holeIndex, topIndex, value);
 }
 
-template <class RandomAccessIterator, class T, class Distance>
-inline void __pop_heap(RandomAccessIterator first, RandomAccessIterator last,
-                       RandomAccessIterator result, T value, Distance*) {
-    *result = *first;
-    __adjust_heap(first, Distance(0), Distance(last - first), value);
-}
-
-template <class RandomAccessIterator, class T>
-inline void __pop_heap_aux(RandomAccessIterator first,
-                           RandomAccessIterator last, T*) {
-    __pop_heap(first, last - 1, last - 1, T(*(last - 1)), distance_type(first));
-}
-
-template <class RandomAccessIterator>
-inline void pop_heap(RandomAccessIterator first, RandomAccessIterator last) {
-    __pop_heap_aux(first, last, value_type(first));
-}
 
 template <class RandomAccessIterator, class Distance, class T, class Compare>
 void __adjust_heap(RandomAccessIterator first, Distance holeIndex,
@@ -113,26 +160,6 @@ void __adjust_heap(RandomAccessIterator first, Distance holeIndex,
     __push_heap(first, holeIndex, topIndex, value, comp);
 }
 
-template <class RandomAccessIterator, class T, class Compare, class Distance>
-inline void __pop_heap(RandomAccessIterator first, RandomAccessIterator last,
-                       RandomAccessIterator result, T value, Compare comp,
-                       Distance*) {
-    *result = *first;
-    __adjust_heap(first, Distance(0), Distance(last - first), value, comp);
-}
-
-template <class RandomAccessIterator, class T, class Compare>
-inline void __pop_heap_aux(RandomAccessIterator first,
-                           RandomAccessIterator last, T*, Compare comp) {
-    __pop_heap(first, last - 1, last - 1, T(*(last - 1)), comp,
-               distance_type(first));
-}
-
-template <class RandomAccessIterator, class Compare>
-inline void pop_heap(RandomAccessIterator first, RandomAccessIterator last,
-                     Compare comp) {
-    __pop_heap_aux(first, last, value_type(first), comp);
-}
 
 template <class RandomAccessIterator, class T, class Distance>
 void __make_heap(RandomAccessIterator first, RandomAccessIterator last, T*,
