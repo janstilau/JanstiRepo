bffcf5a65328eece4dc47c59fecd9c8e5e104dee
JustinLau <janstilau@gmail.com>
2021-03-12 17:59:39 +0800

    import handjson

--- Demo/3RDLibContainer/3RDLibContainer/ViewController.swift
+++ Demo/3RDLibContainer/3RDLibContainer/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Touch begin")
+    }
 
 }
 
